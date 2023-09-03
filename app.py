073d252cb87fb12d7b043b99a47b7774c4df83ae
BigDataMao <mxw712@gmail.com>
2023-09-03 23:24:50 +0800

    第一次与数据库联动成功

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pymysql
 
 # app = Flask(__name__) 这是默认的写法
 # 但是如果我们把这个文件放到了其他的文件夹下面，那么就需要指定一下
@@ -19,6 +20,30 @@ def get_news():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/account', methods=['GET', 'POST'])
+def account():
+    if request.method == 'GET':
+        return render_template('account.html')
+    user = request.form.get('user')
+    passwd = request.form.get('passwd')
+    name = request.form.get('name')
+    gender = request.form.get("gender")
+    city = request.form.get("city")
+    email = request.form.get("email")
+
+    print(user, passwd, name, gender, city, email)
+
+    # 保存到数据库
+    conn = pymysql.connect(host="txy", port=3306, user="root", password="mxw19910712@MYSQL", database="flask", charset="utf8")
+    cursor = conn.cursor()
+    sql = "insert into account values(%s, %s, %s, %s, %s, %s)"
+    cursor.execute(sql, [user, passwd, name, 1, city, email])
+    conn.commit()
+    conn.close()
+
+    return "提交成功"
 
 
 if __name__ == '__main__':
